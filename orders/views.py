94b124bd972b595a0b3174184f9cf1f08dfe09b1
alex <a.n.chasovskoy@gmail.com>
2024-08-30 14:22:56 +0200

    celery task added  and introduced into the order creation view

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,10 @@
-from cart.cart import Cart
 from django.shortcuts import render
+
+from cart.cart import Cart
+
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 
 def order_create(request):
@@ -20,6 +23,11 @@ def order_create(request):
                 )
             # Clear the cart after all items have been processed
             cart.clear()
+
+            # Send an email notification using Celery (delay used to send the notification asynchronously)
+            order_created.delay(order.id)
+
+            # Redirect to a success page
             return render(request, "orders/order/created.html", {"order": order})
     else:
         form = OrderCreateForm()
